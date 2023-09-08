746d602d58bd025e9b0eafcc8d4ee31f91ff43b3
孤之界 <f20001012@gmail.com>
2023-09-08 11:34:59 +0800

    feature: 新增 Migration

---
+++ MuteReborn/Migrations/20230908032655_Init.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MuteReborn.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GuildConfigs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GuildId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    EnableMuteReborn = table.Column<bool>(type: "INTEGER", nullable: false),
+                    BuyMuteRebornTicketCost = table.Column<int>(type: "INTEGER", nullable: false),
+                    EachTicketIncreaseMuteTime = table.Column<int>(type: "INTEGER", nullable: false),
+                    EachTicketDecreaseMuteTime = table.Column<int>(type: "INTEGER", nullable: false),
+                    MaxIncreaseMuteTime = table.Column<int>(type: "INTEGER", nullable: false),
+                    DateAdded = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuildConfigs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MuteRebornTickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    GuildId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    RebornTicketNum = table.Column<int>(type: "INTEGER", nullable: false),
+                    DateAdded = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MuteRebornTickets", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GuildConfigs");
+
+            migrationBuilder.DropTable(
+                name: "MuteRebornTickets");
+        }
+    }
+}
