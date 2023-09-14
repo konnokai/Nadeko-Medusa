00379c9641d8107bb9a814c6bfcbf820261f619a
孤之界 <f20001012@gmail.com>
2023-09-14 18:07:24 +0800

    featute: 移除 HSRUtility 模組

--- HSRUtility/Database/Models/BetGuildConfigs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HSRUtility.Database.Models
-{
-    public class PlayerIdLink : DbEntity
-    {
-        public ulong UserId { get; set; }
-        public string PlayerId { get; set; } = "";
-    }
-}
